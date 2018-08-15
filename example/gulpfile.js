dc880398627ee924d18d34edb364bfec812e8f0f
Tiger_Huang <492122337@qq.com>
2018-08-15 15:42:18 +0800

    1.0.0

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -1,11 +1,8 @@
 var gulp = require('gulp');
-gulpHtmlVersion = require('../index');
+gulpRepeatItem = require('../index');
 
 gulp.task('default', function () {
   return gulp.src('./*.html')
-    .pipe(gulpHtmlVersion({
-      paramName: 'version',
-      suffix: ['css', 'js', 'png'],
-    }))
+    .pipe(gulpRepeatItem())
     .pipe(gulp.dest('./build/'));
 });
